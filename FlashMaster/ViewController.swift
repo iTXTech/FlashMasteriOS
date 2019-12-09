03773bac388492749782ee1fcd4517c6fbbc6254
PeratX <peratx@itxtech.org>
2019-12-08 17:23:29 -0800

    Theme: update

--- FlashMaster/ViewController.swift
+++ FlashMaster/ViewController.swift
@@ -9,14 +9,29 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var webViewContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let webView = WKWebView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
-        self.view.addSubview(webView)
+        webView.scrollView.bounces = false
+        webView.scrollView.delegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        self.webViewContainer.addSubview(webView)
+        
+        webView.topAnchor.constraint(equalTo: webViewContainer.topAnchor).isActive = true
+        webView.rightAnchor.constraint(equalTo: webViewContainer.rightAnchor).isActive = true
+        webView.leftAnchor.constraint(equalTo: webViewContainer.leftAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: webViewContainer.bottomAnchor).isActive = true
+        
         let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "Resource")!
         webView.loadFileURL(url, allowingReadAccessTo: url)
         let request = URLRequest(url: url)
         webView.load(request)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return nil
     }
 }
